85140796d9b2edd6cea3b14c9b4870d7dd5ebcb2
MKlyarovich <mklyarovich@gmail.com>
2019-03-28 00:30:58 +0300

    Revert "Create all-in-all project"
    This reverts commit eb7beb17192512ca2dd956fa1233e0a56a5fd01b.

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Downloads the spring-loaded lib if not existing and runs the full all-in-one
-# (Alfresco + Share + Solr) using the runner project
-springloadedfile=~/.m2/repository/org/springframework/springloaded/1.2.0.RELEASE/springloaded-1.2.0.RELEASE.jar
-
-if [ ! -f $springloadedfile ]; then
-mvn validate -Psetup
-fi
-MAVEN_OPTS="-javaagent:$springloadedfile -noverify -Xms1G -Xmx4G -XX:PermSize=600m" mvn install -Prun
